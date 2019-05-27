b2377621cd672deb77b8901e145eca7d8b06aa49
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-05-27 10:16:54 +0700

    Create Extract Nested Object Reference.js

---
+++ Extract Nested Object Reference.js
@@ -0,0 +1,30 @@
+/*
+Description:
+You are given a complex object that has many deeply nested variables. You don't want to go the usual if obj.property == null route. Create a prototype method that given a nested path, either return the value or undefined.
+
+var obj = {
+  person: {
+    name: 'joe',
+    history: {
+      hometown: 'bratislava',
+      bio: {
+        funFact: 'I like fishing.'
+      }
+    }
+  }
+};
+
+obj.hash('person.name'); // 'joe'
+obj.hash('person.history.bio'); // { funFact: 'I like fishing.' }
+obj.hash('person.history.homeStreet'); // undefined
+obj.hash('person.animal.pet.needNoseAntEater'); // undefined
+*/
+Object.prototype.hash = function(string) {
+    let obj = this;
+    let arr = string.split(".").forEach(function(elem) {
+        if (obj) {
+            obj = obj[elem];
+        }
+    });
+    return obj;
+}
